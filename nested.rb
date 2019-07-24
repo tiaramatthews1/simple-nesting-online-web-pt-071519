79270fd94e964b75ddbc9f72e145e2ef9f78a020
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-24 23:45:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,7 +19,6 @@ return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
